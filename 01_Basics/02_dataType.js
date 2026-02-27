5d10367656dcf945e303e9ad5a4331ce2210ad85
yashwant1606 <ylbpathondiya1606@gmail.com>
2026-02-27 23:29:38 +0530

    DOM one , two and three

--- 01_Basics/02_dataType.js
+++ 01_Basics/02_dataType.js
@@ -14,6 +14,15 @@
   
 
  let Name = "yashwant"
-console.log(Name);
+//console.log(Name);
     
 
+//logical operators
+
+a= 5 //  6 > 5 >4>3 >2 >3
+b = a++ // 5 > 4 > 5
+let c = (--b) + (a--) + (++b) // 4 + 6 + 5 = 15 > 16
+let d = (++c) + 20 + (--a) + (a--) // 16 + 20 + 4 + 4 >44 >43
+let e = (--a) + (--d) + (a++)  // 43 + 2 + 2
+
+console.log(a,b ,c,d,e);
